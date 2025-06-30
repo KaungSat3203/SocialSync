e92f67487d3ff099f413986d49f81c8914e5a31d
KaungSat3203 <kaungsat3203@gmail.com>
2025-06-30 22:44:14 +0700

    fixing build error

--- frontend/app/hooks/auth/useOAuthLogin.js
+++ frontend/app/hooks/auth/useOAuthLogin.js
@@ -1,4 +1,6 @@
-export const useOAuthLogin = (baseUrl = 'http://localhost:8080') => {
+export const useOAuthLogin = () => {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+
   const loginWithFacebook = () => {
     window.location.href = `${baseUrl}/auth/facebook/login`;
   };
